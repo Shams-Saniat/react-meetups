7249aaed9c205c1b3c6ac55bf004eee92f3f65c4
Shams Saniat <shams.saniat@northsouth.edu>
2025-07-17 13:04:01 +0600

    Added links, Navigation navbar, outputting lists

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router"
 import AllMeetupsPage from "./pages/AllMeetups"
 import NewMeetupPage from "./pages/NewMeetups"
 import FavouritesPage from "./pages/Favourites"
+import MainNavigation from "./components/MainNavigation"
 
 function App() {
 
   return <div>
+    <MainNavigation />
     <Routes>
       <Route path="/" element={<AllMeetupsPage />} />
       <Route path="/new-meetup" element={<NewMeetupPage />} />
-      <Route path='/favorites' element={<FavouritesPage />} />
+      <Route path='/favourites' element={<FavouritesPage />} />
     </Routes>
   </div>
 }
--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,7 +1,18 @@
-function MainNavigation(){
+import { Link } from 'react-router-dom';
+function MainNavigation() {
     return <header>
-        <div>
-            
+
+        <div className="navbar bg-cyan-500 shadow-sm">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">React Meetups</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><Link to='/'>All Meetups</Link></li>
+                    <li><Link to='/new-meetup'>Add New Meetup</Link></li>
+                    <li><Link to='/favourites'>My Favourites</Link></li>
+                </ul>
+            </div>
         </div>
     </header>
 }
